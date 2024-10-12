376ac8aac07b0cef2c5a6be72c6cfb8cd51cb6c3
geekydeepanshu <saini1234deepanshu@gmail.com>
2024-10-12 13:37:32 +0530

    modified: store.js  export defaut syntax imporoved

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,9 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice.js";
 import themeReducer from "./themeSlice.js";
 
-export default store = configureStore({
+const store = configureStore({
     reducer: {
         auth:authReducer,
         theme:themeReducer,
     }
+
+    // concept of combine reducer
 });
+
+export default store;
