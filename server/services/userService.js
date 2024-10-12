77c1c610429f25c1961df7cb0e799ee7dbfa006d
Abhishek9812 <competeabhi@gmail.com>
2024-10-12 17:17:18 +0530

    implement token verification in register and login

--- server/services/userService.js
+++ server/services/userService.js
@@ -2,27 +2,41 @@ const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
+const generateToken = require('../utils/generateToken');
 
 // Register a new user
-const registerUser = asyncHandler(async (username, email, password) => {
+const registerUser = async (username, email, password) => {
     // Check if user already exists
     const userExists = await User.findOne({ email });
     if (userExists) {
         throw new Error('User already exists');
     }
 
-    // Create new user
-    const user = new User({ username, email, password });
-    await user.save();
-    return user;
-});
+    // Create a new user and save it to the database
+    const user = new User({
+        username,
+        email,
+        password
+    });
+
+    // Save the user
+    const savedUser = await user.save();
+
+    // Return user data along with generated token
+    return {
+        _id: savedUser._id,
+        username: savedUser.username,
+        email: savedUser.email,
+        token: generateToken(savedUser._id), // Generate JWT token
+    };
+};
 
 // Login a user
 const loginUser = asyncHandler(async (email, password) => {
     const user = await User.findOne({ email });
     if (user && (await bcrypt.compare(password, user.password))) {
         // Generate a JWT token
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = generateToken(user._id);
         return { user, token };
     } else {
         throw new Error('Invalid email or password');
